5fb54f870b0f689ee7b8af9bc08706f1be19562e
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-04-04 20:50:28 +0500

    Update class.py

--- pythin Paractice/class.py
+++ pythin Paractice/class.py
@@ -1,3 +1,6 @@
-d={'cat':'billidibachi','ghora':'ghori','insan':'janwar'}
-d.update({'cat':'dog'})
-print(d)
+# d={'cat':'billidibachi','ghora':'ghori','insan':'janwar'}
+# d.update({'cat':'dog'})
+# print(d)
+nums = [0, 1, 2, 3, 4]
+squares = [x**2 for x in nums]
+print(squares)
